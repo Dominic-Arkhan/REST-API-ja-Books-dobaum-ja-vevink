f59d46fe957d7a53cfb622593eaf705d49c41fca
Dominic-Arkhan <161318755+Dominic-Arkhan@users.noreply.github.com>
2026-03-29 09:52:39 +0000

    adding mockdata and in memory storage to index file

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,3 +3,5 @@ export * from "./book.model";
 export * from "./genre.model";
 export * from "./publisher.model";
 export * from "./review.model";
+export * from "./inMemoryStore";
+export * from "./mockData";
