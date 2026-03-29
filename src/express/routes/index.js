c0c559a4179121b2a913bfc511e80ba6196e4ae1
Veronica Vinkler <161318939+veronicavinkler@users.noreply.github.com>
2026-03-29 11:22:11 +0000

    creating index file for routes

---
+++ src/express/routes/index.js
@@ -0,0 +1,13 @@
+const authorsRoutes = require('./routes.authors');
+const booksRoutes = require('./routes.books');
+const genreRoutes = require('./routes.genre');
+const publishersRoutes = require('./routes.publishers');
+const reviewsRoutes = require('./routes.reviews');
+
+module.exports = {
+    authorsRoutes,
+    booksRoutes,
+    genreRoutes,
+    publishersRoutes,
+    reviewsRoutes
+};
